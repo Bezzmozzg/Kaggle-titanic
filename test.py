f1470e554ad239f8af3638be1cfd4ea5c2452efb
Bezzmozzg <38427951+Bezzmozzg@users.noreply.github.com>
2020-09-03 12:40:03 +0500

    ~~~

---
+++ test.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from flask import Flask, jsonify
+
+df_t = pd.read_csv('titanic.csv')
+
+result = df_t.query("Survived == 1 and Age > 18").sort_values('Age').head(10)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return result.to_html()
+
+
+@app.route('/data.json')
+def data():
+    return df_t.to_json()
+
+
+if __name__ == '__main__':
+    app.run()
